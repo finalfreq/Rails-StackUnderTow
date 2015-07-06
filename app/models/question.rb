627c3b71412a948547c4fbb0d26ddb23372c0de0
Adam Mcfadden & james williams <student@epicodus.com>
2015-07-06 12:23:35 -0700

    add question model, and spec to validate name and body of question.

---
+++ app/models/question.rb
@@ -0,0 +1,4 @@
+class Question < ActiveRecord::Base
+  validates :name, presence: true
+  validates :body, presence: true
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
     email('betty@gmail.com')
     password('badpass123')
   end
+
+  factory(:question) do
+    name('what is foo?')
+    body('where the hell does foo bar come from?')
+  end
 end
