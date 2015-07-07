bd3c394d00472f980d5d7235a53b0060a8bfe163
Adam McFadden & James Robert Williams <student@epicodus.com>
2015-07-07 14:44:31 -0700

    factory girl associations fixed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,10 +6,9 @@ describe Answer do
   it {should belong_to :user}
 
   it "sends an email when the answer is posted" do
-    user = FactoryGirl.create(:user, name: 'name', email: 'email@email.com')
     question = FactoryGirl.create(:question)
     answer = FactoryGirl.create(:answer)
-    user.questions.push(question)
+    user = question.user
     user.answers.push(answer)
     question.answers.push(answer)
     answer.answer_email
